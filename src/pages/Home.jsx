0ba08a2cd8592d77eb5e5b3bbbf6f9e99e05aca0
iluvyw <anhoangpham2001@gmail.com>
2022-01-24 15:49:52 +0700

    minor fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,6 @@ import Video from '../assets/intro.mp4'
 import './Home.scss'
 
 export default function Home() {
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
     const videoRef = useRef(null)
 
     useEffect(() => {
